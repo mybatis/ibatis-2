34b8758bcbf4862bcfa51dead419c9395535bd67
Jeremy Landis <jeremylandis@hotmail.com>
2024-09-25 20:14:55 -0400

    Update NodeletParser.java

--- src/main/java/com/ibatis/common/xml/NodeletParser.java
+++ src/main/java/com/ibatis/common/xml/NodeletParser.java
@@ -199,9 +199,6 @@ public class NodeletParser {
     factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
     factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
     factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
-    factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-    factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
-    factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
     factory.setValidating(validation);
 
     factory.setNamespaceAware(false);
