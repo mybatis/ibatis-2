a9600b45e1622b14fe603a0ecb80951821c03bb8
Jeremy Landis <jeremylandis@hotmail.com>
2025-05-26 15:19:02 -0400

    Make static class private constructor

--- src/main/java/com/ibatis/sqlmap/engine/type/SimpleDateFormatter.java
+++ src/main/java/com/ibatis/sqlmap/engine/type/SimpleDateFormatter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2023 the original author or authors.
+ * Copyright 2004-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,13 @@ import java.util.Date;
  * The Class SimpleDateFormatter.
  */
 public class SimpleDateFormatter {
+
+  /**
+   * Prevent instantiation.
+   */
+  private SimpleDateFormatter() {
+    // Prevent instantiation
+  }
 
   /**
    * Format.
