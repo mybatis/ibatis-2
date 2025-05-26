3c57e28ba70109577c83b30f9dcad8d01003ed17
Jeremy Landis <jeremylandis@hotmail.com>
2025-05-26 15:19:35 -0400

    Fix modifier orders

--- src/main/java/com/ibatis/sqlmap/engine/type/CallableStatementResultSet.java
+++ src/main/java/com/ibatis/sqlmap/engine/type/CallableStatementResultSet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2022 the original author or authors.
+ * Copyright 2004-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -461,11 +461,11 @@ public class CallableStatementResultSet implements ResultSet {
     throw new UnsupportedOperationException("CallableStatement does not support this method.");
   }
 
-  public void updateBytes(int columnIndex, byte x[]) throws SQLException {
+  public void updateBytes(int columnIndex, byte[] x) throws SQLException {
     throw new UnsupportedOperationException("CallableStatement does not support this method.");
   }
 
-  public void updateBytes(String columnName, byte x[]) throws SQLException {
+  public void updateBytes(String columnName, byte[] x) throws SQLException {
     throw new UnsupportedOperationException("CallableStatement does not support this method.");
   }
 
