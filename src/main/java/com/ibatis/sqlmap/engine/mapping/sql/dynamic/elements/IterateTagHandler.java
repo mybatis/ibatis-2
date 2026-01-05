a9769f7acc9833ff73ec84cfa03969a8a75763ba
Jeremy Landis <jeremylandis@hotmail.com>
2026-01-04 21:11:00 -0500

    Use proper isEmpty instead of check on ""

--- src/main/java/com/ibatis/sqlmap/engine/mapping/sql/dynamic/elements/IterateTagHandler.java
+++ src/main/java/com/ibatis/sqlmap/engine/mapping/sql/dynamic/elements/IterateTagHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2025 the original author or authors.
+ * Copyright 2004-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,7 @@ public class IterateTagHandler extends BaseTagHandler {
 
       Object collection;
       String prop = tag.getPropertyAttr();
-      if (prop != null && !prop.equals("")) {
+      if (prop != null && !prop.isEmpty()) {
         if (null != parentIterate && parentIterate.isAllowNext()) {
           parentIterate.next();
           parentIterate.setAllowNext(false);
